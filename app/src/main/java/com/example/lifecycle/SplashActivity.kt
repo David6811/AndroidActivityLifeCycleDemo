8f9162d4ba692760432e637cd8fcee4e78c865ac
weixu <xuwei19850423@gmail.com>
2025-08-09 09:50:14 +1000

    Enable Splash

---
+++ app/src/main/java/com/example/lifecycle/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lifecycle
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : Activity() {
+
+    companion object {
+        private const val TAG = "SplashActivity_Lifecycle"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+        
+        splashScreen.setKeepOnScreenCondition { false }
+        
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    override fun onStart() { super.onStart(); Log.d(TAG, "onStart") }
+    override fun onResume() { super.onResume(); Log.d(TAG, "onResume") }
+    override fun onPause() { super.onPause(); Log.d(TAG, "onPause") }
+    override fun onStop() { super.onStop(); Log.d(TAG, "onStop") }
+    override fun onRestart() { super.onRestart(); Log.d(TAG, "onRestart") }
+    override fun onDestroy() { super.onDestroy(); Log.d(TAG, "onDestroy") }
+}
